5c6912715fc97b721999d7dc22a178ec1c7b4718
NYARCAL <calvinnyakwe@gmail.com>
2022-09-09 08:10:21 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,23 +5,10 @@
  * Return: Always 0.
  */
 int main(void)
-  
 {
-  
-  char letter;
-  
-
-  
-  for (letter = 'a'; letter <= 'z'; letter++)
-    
-    putchar(letter);
-  
-
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
