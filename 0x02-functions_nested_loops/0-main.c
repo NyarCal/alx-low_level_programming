aa841c686feb913c34da31d5e81ad6cb68d789fd
NYARCAL <calvinnyakwe@gmail.com>
2022-09-14 11:46:34 -0700

    put char

--- 0x02-functions_nested_loops/0-main.c
+++ 0x02-functions_nested_loops/0-main.c
@@ -1,12 +1,38 @@
 #include "main.h"
 /**
- * main - Prints character
+
+ * main - Prints Holberton
+
  *
+
  * Return: Always 0 (Success)
+
  */
+
 int main(void)
+  
 {
-_putchar();
-_putchar('\n');
-return (0);
+  
+  _putchar('H');
+  
+  _putchar('o');
+  
+  _putchar('l');
+  
+  _putchar('b');
+  
+  _putchar('e');
+  
+  _putchar('r');
+  
+  _putchar('t');
+  
+  _putchar('o');
+  
+  _putchar('n');
+  
+  _putchar('\n');
+  
+  return (0);
+  
 }
