f1f91d6e11b0a8a4ccb8c6dac558def6dd74997d
NYARCAL <calvinnyakwe@gmail.com>
2022-09-19 12:16:41 -0700

    print half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,21 @@ void puts_half(char *str)
 {
 int i, j, max;
 for (i = 0; str[i] != '\0'; i++)
+{
 if ((i % 2) == 0)
 {
 for (j=i /2; str[j] != '\0'; j++)
+{
 putchar(str[j];
+}
 }
 else
 {
 for (max = (i-1)/2; str[max] != '\0'; max++)
+{
 putchar(str[max + 1];
+}
+}
 }
 putchar('\n');
 }
