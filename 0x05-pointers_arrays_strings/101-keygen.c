16d2be7b9c9c5de67fe2852f2e2add49bb3b01b4
NYARCAL <calvinnyakwe@gmail.com>
2022-09-20 06:13:45 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 #include <time.h>
-
 #include <stdlib.h>
-
 int password(int password_length) {
-  
 char list[] = "1234567890abcdefghijklmnoqprstuvwyzxABCDEFGHIJKLMNOQPRSTUYWVZX!@#$^&*?";
 srand(time(NULL));
-  
-for(int i = 0; i < password_length; i++) {
-    
+for(int i = 0; i < password_length; i++)
+{
 printf("%c", list[rand() % (sizeof list - 1)]);
-    
 }
 }
