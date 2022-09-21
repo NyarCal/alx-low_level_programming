1113451a4b8a45b041f7cfafdb1af615e6dcd446
NYARCAL <calvinnyakwe@gmail.com>
2022-09-21 06:46:23 -0700

    concatenates two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
-defnif main_h
-define main_h
+#ifndef main_h
+#define main_h
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,4 +11,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
-endf /* main.h */
+#endif /* main.h */
