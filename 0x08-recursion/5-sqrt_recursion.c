01f7e876ffac8b930496bc416db48c85888df43a
NYARCAL <calvinnyakwe@gmail.com>
2022-09-28 04:54:52 -0700

    square roof

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,3 +12,18 @@ return (-1);
 else
 return (squareroot(n, (n + 1) / 2));
 }
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squareroot(int n, int i)
+{
+if (i < 1)
+return (-1);
+else if (i * i == n)
+return (i);
+else
+return (squareroot(n, i - 1));
+}
