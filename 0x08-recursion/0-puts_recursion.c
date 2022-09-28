4a9da88bb07784400f00d8df63bb087cae5c1c82
NYARCAL <calvinnyakwe@gmail.com>
2022-09-28 02:28:40 -0700

    recursive printing of string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - check the string to be printed
+ * @t: string to be printed
+ * Return: Always 0.
+ */
+void _puts_recursion(char *t) {
+if (*t == '\0')
+return;
+printf("%c", *t);
+print(++t);
+}
