129a0853b6da7edd1e18334b0f09fe4e4203b1ed
NYARCAL <calvinnyakwe@gmail.com>
2022-09-28 06:46:37 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,7 +42,7 @@ int palindrome(char str[], int st, int end)
 {
 if (st >= end)
 return (1);
-if (str[st] != str[end])    
+if (str[st] != str[end])
 return (0);
 if (st <= end || st < end + 1)
 return (palindrome(str, st + 1, end - 1));
