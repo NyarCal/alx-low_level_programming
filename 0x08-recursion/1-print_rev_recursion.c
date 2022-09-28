0ed7c4b8263ad255fa035f026b324f273b5bc222
NYARCAL <calvinnyakwe@gmail.com>
2022-09-28 01:57:32 -0700

    reverse string updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * _print_rev_recursion - check the code
+ *@str: String to be reversed
  * Return: Always 0.
  */
 void _print_rev_recursion(char *str)
