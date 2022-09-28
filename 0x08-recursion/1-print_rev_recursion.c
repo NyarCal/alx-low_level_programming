1bd849a1caccef171fd83482e854e77125e073d9
NYARCAL <calvinnyakwe@gmail.com>
2022-09-28 02:00:29 -0700

    reverse string updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,9 @@
  */
 void _print_rev_recursion(char *str)
 {
-  if(*str)
-    {
-      _print_rev_recursion(str + 1);
-      printf("%c", *str);
-    }
+if(*str)
+{
+_print_rev_recursion(str + 1);
+printf("%c", *str);
+}
 }
