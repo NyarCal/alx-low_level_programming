af0745a0816d4e907794e1d1db51fd22485a74a2
NYARCAL <calvinnyakwe@gmail.com>
2022-09-30 14:39:32 -0700

    lib1

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
- _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Calvin Nyakweba");
- return (0);
+puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Calvin Nyakweba");
+return (0);
 }
