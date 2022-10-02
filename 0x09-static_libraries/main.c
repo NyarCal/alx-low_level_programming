d7008c4075d5d83c7d04a24d7de717cc56c95311
NYARCAL <calvinnyakwe@gmail.com>
2022-10-02 08:22:27 -0700

    static library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 int main(void)
 {
-puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Calvin Nyakweba");
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Calvin Nyakweba");
 return (0);
 }
