282385e1f80de16c8871df1e1d120c320606e4fc
NYARCAL <calvinnyakwe@gmail.com>
2022-11-02 13:21:04 -0700

    update read and create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-**
+/**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
@@ -12,10 +12,8 @@
 int create_file(const char *filename, char *text_content)
 {
 int o, w, len = 0;
- 
 if (filename == NULL)
 return (-1);
-
 if (text_content != NULL)
 {
 for (len = 0; text_content[len];)
